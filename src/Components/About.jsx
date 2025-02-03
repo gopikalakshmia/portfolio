c135eb070240779da22e0b9f19956c1e21a4cc57
gopika <gopikalakshmia@gmail.com>
2025-02-03 13:56:32 -0500

    Added experience and changes in about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,14 +1,17 @@
 function About() {
     return ( <div className=" bg-black text-white text-left flex">
-        <div className=" p-10 m-1.5 w-1/2">
+        <div className=" p-10 m-1.5 pr-5 w-1/2 text-center" >
         <h1 className="text-center"> About Me</h1>
         <p>With five years of experience as a software developer at Cognizant, I have honed my skills in ASP.NET development, React development, and more. I am confident that my expertise aligns with your organization’s needs and would contribute positively to your team.
          </p>
+         <br/>
          <p>During my tenure at Cognizant, I worked as an ASP.NET developer, utilizing HTML, SQL, ASP.NET, SSRS, and TypeScript to develop applications. I also played a crucial role in automating reporting processes, significantly enhancing organizational efficiency. </p>
+         <br/>
          <p>After taking a career break to prioritize my family, I pursued certifications in UiPath and MEAN Stack development. Additionally, I gained valuable experience as a freelance React developer, crafting user-friendly applications. Currently, I am actively engaged in enhancing my skills by enrolling in advanced React courses.</p>
+         <br/>
          <p>I’m ready to return to the tech industry and apply my skills in developing innovative web solutions. I’m looking forward to joining a dynamic team and contributing to exciting projects.</p>
         </div>
-        <div className=" p-4 w-1/2 ">
+        <div className=" p-4 w-1/2  pl-6">
             <img src={'https://cdni.iconscout.com/illustration/premium/thumb/female-web-developer-writing-code-lines-illustration-download-in-svg-png-gif-file-formats--website-development-programming-business-activities-pack-illustrations-5501007.png'}/>
         </div>
        
